c5e95f4b1e631d4c6cb0bfe54a1b5285b9245371
sooryakumar <sooryakumar.v@heptagon.in>
2019-06-03 07:50:12 +0530

    Migrations

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+	name = models.CharField(max_length=200)
+	email_id = models.EmailField(max_length=254)
+	password = models.TextField()
+	is_store_man = models.SmallIntegerField(default=0)
+	active_flag = models.SmallIntegerField(default=1)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+class PriorityMaster(models.Model):
+	priority = models.CharField(max_length=50)
+	active_flag = models.SmallIntegerField(default=1)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+class TaskStateMaster(models.Model):
+	state = models.CharField(max_length=50)
+	active_flag = models.SmallIntegerField(default=1)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+class Tasks(models.Model):
+	title = models.CharField(max_length=200)
+	task_state = models.ForeignKey(TaskStateMaster, on_delete=models.CASCADE)
+	priority = models.ForeignKey(PriorityMaster, on_delete=models.CASCADE)
+	employee = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='+')
+	created_by = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='+')
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
