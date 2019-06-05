ab3709aaa80c52303c20fe2a2806ecd3b73fc705
sooryakumar <sooryakumar.v@heptagon.in>
2019-06-05 12:51:55 +0530

    Websocket for live updation of lists

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.list, name='list'),
+    path('<id>', views.view_task, name='view_task'),
     path('create', views.create, name='create'),
     path('store', views.store, name='store'),
+    path('cancel/<id>', views.cancel, name='cancel'),
     # url(r'^$', views.index, name='index'),
-    url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+    # url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
 ]
